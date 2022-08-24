e155ee5f864da3c098bf0ea926b860d2fe436bbb
irenavilbik <i_vilbik@yahoo.com>
2022-08-24 14:10:37 +0300

    Update

--- Animals/Main.java
+++ Animals/Main.java
@@ -1,25 +1,18 @@
-package OOP.Animals;
+package inheritance.Animals;
 
 public class Main {
 
     public static void main(String[] args) {
 
-                Animal dog = new Animal();
-                dog.Name = "Dollar";
                 Cat kitty = new Cat();
                 kitty.Name = "Michelle";
                 kitty.eat();
-                dog.eat();
                 kitty.sayYourName();
                 Breed british = new Breed();
                 british.Name = "Churchill";
                 british.breed = "British Shorthair";
                 british.sayYourBreed();
                 british.sayYourName();
-
-
-
-
     }
 
 
